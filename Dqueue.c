c9fd122faed586ae6ab2f1f111eba22fdbabc3b2
Krishna-Tailor <ks.tailor14@gmail.com>
2026-05-29 22:15:08 +0530

    Made Functinons of Input And Delete For Every Operation

--- Dqueue.c
+++ Dqueue.c
@@ -4,13 +4,19 @@
 int deQueue[SIZE], f = -1, r = -1;
 
 void InputRestricted();
-void IRRear();
 void IRFront();
+void IInput();
+void IRRear();
+void RInput();
+
+void DeleteFromFront();
+void DeleteFromRear();
 
 void OutputRestricted();
 void ORFront();
 void ORRear();
 
+void Display();
 
 int main()
 {
@@ -70,7 +76,7 @@ void InputRestricted()
 
 void IRFront()
 {
-    int choice, element;
+    int choice;
     do
     {
         printf("\n\n");
@@ -81,117 +87,24 @@ void IRFront()
         switch (choice)
         {
         case 1:
-            if (f == -1 && r == -1)
-            {
-                f = r = 0;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[f] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****\n\n", element);
-            }
-
-            else if (f == r + 1 || f == 0 && r == SIZE - 1)
-            {
-                system("clear");
-                printf("**** Queue Is Full ****");
-            }
-
-            else if (f == 0)
-            {
-                f = SIZE - 1;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[f] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****\n\n", element);
-            }
-
-            else
-            {
-                f--;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[f] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****\n\n", element);
-            }
+            IInput(); // Takes Input Of Element And Stores In Front Of The Queue
             break;
 
         case 2:
-            if (f == -1 && r == -1)
-            {
-                system("clear");
-                printf("**** Queue Is Empty ****");
-                break;
-            }
-
-            else if (f == r)
-            {
-                system("clear");
-                printf("**** Element %d Deleted Successfully ****", deQueue[f]);
-                f = r = -1;
-                break;
-            }
-
-            else if (f == SIZE - 1)
-            {
-                system("clear");
-                printf("**** Element %d Deleted Successfully ****", deQueue[f]);
-                f = 0;
-                break;
-            }
-
-            system("clear");
-            printf("**** Element %d Deleted Successfully ****", deQueue[f]);
-            f++;
+            DeleteFromFront();
             break;
 
         case 3:
-            if (f == -1 && r == -1)
-            {
-                system("clear");
-                printf("**** Queue Is Empty ****");
-                break;
-            }
-
-            else if (f == r)
-            {
-                system("clear");
-                printf("**** Element %d Deleted Successfully ****", deQueue[r]);
-                deQueue[r] = 0;
-                f = r = -1;
-                break;
-            }
-
-            else if (r == 0)
-            {
-                system("clear");
-                printf("**** Element %d Deleted Successfully ****", deQueue[r]);
-                deQueue[r] = 0;
-                r = SIZE - 1;
-                break;
-            }
-
-            system("clear");
-            printf("**** Element %d Deleted Successfully ****", deQueue[r]);
-            deQueue[r] = 0;
-            r--;
-
+            DeleteFromRear();
             break;
 
         case 4:
-            system("clear");
-            for (int i = 0; i < SIZE; i++)
-                printf("%d ", deQueue[i]);
+            Display();
             break;
 
         case 5:
             system("clear");
-            printf("Exiting....");
+            printf("Exiting....\n");
             break;
 
         default:
@@ -201,9 +114,117 @@ void IRFront()
     } while (choice != 5);
 }
 
+void IInput()
+{
+    int element;
+
+    if (f == -1 && r == -1)
+    {
+        f = r = 0;
+        printf("\nEnter Your Element: ");
+        scanf("%d", &element);
+
+        deQueue[f] = element;
+        system("clear");
+        printf("**** Element %d Inserted Successfully ****\n\n", element);
+    }
+
+    else if (f == r + 1 || f == 0 && r == SIZE - 1)
+    {
+        system("clear");
+        printf("**** Queue Is Full ****");
+    }
+
+    else if (f == 0)
+    {
+        f = SIZE - 1;
+        printf("\nEnter Your Element: ");
+        scanf("%d", &element);
+
+        deQueue[f] = element;
+        system("clear");
+        printf("**** Element %d Inserted Successfully ****\n\n", element);
+    }
+
+    else
+    {
+        f--;
+        printf("\nEnter Your Element: ");
+        scanf("%d", &element);
+
+        deQueue[f] = element;
+        system("clear");
+        printf("**** Element %d Inserted Successfully ****\n\n", element);
+    }
+}
+
+void DeleteFromFront()
+{
+    if (f == -1 && r == -1)
+    {
+        system("clear");
+        printf("**** Queue Is Empty ****");
+        return;
+    }
+
+    else if (f == r)
+    {
+        system("clear");
+        printf("**** Element %d Deleted Successfully ****", deQueue[f]);
+        f = r = -1;
+        return;
+    }
+
+    else if (f == SIZE - 1)
+    {
+        system("clear");
+        printf("**** Element %d Deleted Successfully ****", deQueue[f]);
+        f = 0;
+        return;
+    }
+
+    system("clear");
+    printf("**** Element %d Deleted Successfully ****", deQueue[f]);
+    f++;
+    return;
+}
+
+void DeleteFromRear()
+{
+    if (f == -1 && r == -1)
+    {
+        system("clear");
+        printf("**** Queue Is Empty ****");
+        return;
+    }
+
+    else if (f == r)
+    {
+        system("clear");
+        printf("**** Element %d Deleted Successfully ****", deQueue[r]);
+        deQueue[r] = 0;
+        f = r = -1;
+        return;
+    }
+
+    else if (r == 0)
+    {
+        system("clear");
+        printf("**** Element %d Deleted Successfully ****", deQueue[r]);
+        deQueue[r] = 0;
+        r = SIZE - 1;
+        return;
+    }
+
+    system("clear");
+    printf("**** Element %d Deleted Successfully ****", deQueue[r]);
+    deQueue[r] = 0;
+    r--;
+}
+
 void IRRear()
 {
-    int choice, element;
+    int choice;
     do
     {
 
@@ -215,124 +236,86 @@ void IRRear()
         switch (choice)
         {
         case 1:
-            if (f == -1 && r == -1)
-            {
-                f = r = 0;
-
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-                deQueue[r] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****\n\n", element);
-            }
-
-            else if (r == SIZE - 1 && f != 0)
-            {
-                r = 0;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[r] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****", element);
-            }
-
-            else if (r == SIZE - 1 || r == f - 1)
-            {
-                system("clear");
-                printf("\n ******** Queue Is Full ********");
-            }
-
-            else if (r < f)
-            {
-                r++;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[r] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****", element);
-            }
-
-            else
-            {
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                r++;
-                deQueue[r] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****", element);
-            }
+            RInput();
             break;
 
         case 2:
             system("clear");
-            if (f == -1 && r == -1)
-            {
-                printf("**** Now Queue Is Empty ****");
-                break;
-            }
-
-            else if (f == r)
-            {
-                printf("**** Element %d Deleted Successfully ****", deQueue[f]);
-                deQueue[f] = 0;
-                break;
-            }
-
-            printf("**** Element %d Deleted Successfully ****", deQueue[f]);
-            f++;
+            DeleteFromFront();
             break;
 
         case 3:
-            if (f == -1 && r == -1)
-            {
-                system("clear");
-                printf("**** Queue Is Empty ****");
-                break;
-            }
-
-            else if (f == r)
-            {
-                system("clear");
-                printf("**** Element %d Deleted Successfully ****", deQueue[r]);
-                deQueue[r] = 0;
-                f = r = -1;
-                break;
-            }
-
-            else if (r == 0)
-            {
-                system("clear");
-                printf("**** Element %d Deleted Successfully ****", deQueue[r]);
-                deQueue[r] = 0;
-                r = SIZE - 1;
-                break;
-            }
-
-            system("clear");
-            printf("**** Element %d Deleted Successfully ****", deQueue[r]);
-            deQueue[r] = 0;
-            r--;
-
+            DeleteFromRear();
             break;
 
         case 4:
-            system("clear");
-            for (int i = f; i <= r; i++)
-                printf("%d ", deQueue[i]);
+            Display();
             break;
 
         case 5:
             system("clear");
-            printf("Exiting....");
+            printf("Exiting....\n");
             break;
 
         default:
             break;
         }
     } while (choice != 5);
+}
+
+void RInput()
+{
+    int element;
+
+    if (f == -1 && r == -1)
+    {
+        f = r = 0;
+
+        printf("\nEnter Your Element: ");
+        scanf("%d", &element);
+        deQueue[r] = element;
+        system("clear");
+        printf("**** Element %d Inserted Successfully ****\n\n", element);
+    }
+
+    else if (r == SIZE - 1 && f != 0)
+    {
+        r = 0;
+        printf("\nEnter Your Element: ");
+        scanf("%d", &element);
+
+        deQueue[r] = element;
+        system("clear");
+        printf("**** Element %d Inserted Successfully ****", element);
+    }
+
+    else if (r == SIZE - 1 || r == f - 1)
+    {
+        system("clear");
+        printf("\n ******** Queue Is Full ********");
+    }
+
+    else if (r < f)
+    {
+        r++;
+        printf("\nEnter Your Element: ");
+        scanf("%d", &element);
+
+        deQueue[r] = element;
+        system("clear");
+        printf("**** Element %d Inserted Successfully ****", element);
+    }
+
+    else
+    {
+        printf("\nEnter Your Element: ");
+        scanf("%d", &element);
+
+        r++;
+        deQueue[r] = element;
+        system("clear");
+        printf("**** Element %d Inserted Successfully ****", element);
+    }
 }
 
 void OutputRestricted()
@@ -371,135 +354,19 @@ void ORFront()
         switch (choice)
         {
         case 1:
-            if (f == -1 && r == -1)
-            {
-                f = r = 0;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[f] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****\n\n", element);
-            }
-
-            else if (f == r + 1 || f == 0 && r == SIZE - 1)
-            {
-                system("clear");
-                printf("**** Queue Is Full ****");
-            }
-
-            else if (f == 0)
-            {
-                f = SIZE - 1;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[f] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****\n\n", element);
-            }
-
-            else
-            {
-                f--;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[f] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****\n\n", element);
-            }
+            IInput();
             break;
 
         case 2:
-            if (f == -1 && r == -1)
-            {
-                f = r = 0;
-
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-                deQueue[r] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****\n\n", element);
-            }
-
-            else if (r == SIZE - 1 && f != 0)
-            {
-                r = 0;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[r] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****", element);
-            }
-
-            else if (r == SIZE - 1 || r == f - 1)
-            {
-                system("clear");
-                printf("\n ******** Queue Is Full ********");
-            }
-
-            else if (r < f)
-            {
-                r++;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[r] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****", element);
-            }
-
-            else
-            {
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                r++;
-                deQueue[r] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****", element);
-            }
+            RInput();
             break;
 
         case 3:
-            if (f == -1 && r == -1)
-            {
-                system("clear");
-                printf("**** Queue Is Empty ****");
-                break;
-            }
-
-
-            else if (f == r)
-            {
-                system("clear");
-                printf("**** Element %d Deleted Successfully ****", deQueue[f]);
-                deQueue[f] = 0;
-                f = r = -1;
-                break;
-            }
-
-            else if (f == SIZE - 1)
-            {
-                system("clear");
-                printf("**** Element %d Deleted Successfully ****", deQueue[f]);
-                deQueue[f] = 0;
-                f = 0;
-                break;
-            }
-
-            system("clear");
-            printf("**** Element %d Deleted Successfully ****", deQueue[f]);
-            deQueue[f] = 0;
-            f++;
+            DeleteFromFront();
             break;
 
         case 4:
-            system("clear");
-            for (int i = 0; i < SIZE; i++)
-                printf("%d ", deQueue[i]);
+            Display();
             break;
 
         case 5:
@@ -527,135 +394,19 @@ void ORRear()
         switch (choice)
         {
         case 1:
-            if (f == -1 && r == -1)
-            {
-                f = r = 0;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[f] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****\n\n", element);
-            }
-
-            else if (f == r + 1 || f == 0 && r == SIZE - 1)
-            {
-                system("clear");
-                printf("**** Queue Is Full ****");
-            }
-
-            else if (f == 0)
-            {
-                f = SIZE - 1;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[f] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****\n\n", element);
-            }
-
-            else
-            {
-                f--;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[f] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****\n\n", element);
-            }
+            IInput();
             break;
 
         case 2:
-            if (f == -1 && r == -1)
-            {
-                f = r = 0;
-
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-                deQueue[r] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****\n\n", element);
-            }
-
-            else if (r == SIZE - 1 && f != 0)
-            {
-                r = 0;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[r] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****", element);
-            }
-
-            else if (r == SIZE - 1 || r == f - 1)
-            {
-                system("clear");
-                printf("\n ******** Queue Is Full ********");
-            }
-
-            else if (r < f)
-            {
-                r++;
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                deQueue[r] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****", element);
-            }
-
-            else
-            {
-                printf("\nEnter Your Element: ");
-                scanf("%d", &element);
-
-                r++;
-                deQueue[r] = element;
-                system("clear");
-                printf("**** Element %d Inserted Successfully ****", element);
-            }
+            RInput();
             break;
 
         case 3:
-            if (f == -1 && r == -1)
-            {
-                system("clear");
-                printf("**** Queue Is Empty ****");
-                break;
-            }
-
-            else if (f == r)
-            {
-                system("clear");
-                printf("**** Element %d Deleted Successfully ****", deQueue[r]);
-                deQueue[r] = 0;
-                f = r = -1;
-                break;
-            }
-
-            else if (r == 0)
-            {
-                system("clear");
-                printf("**** Element %d Deleted Successfully ****", deQueue[r]);
-                deQueue[r] = 0;
-                r = SIZE - 1;
-                break;
-            }
-
-            system("clear");
-            printf("**** Element %d Deleted Successfully ****", deQueue[r]);
-            deQueue[r] = 0;
-            r--;
-
+            DeleteFromRear();
             break;
 
         case 4:
-            system("clear");
-            for (int i = 0; i < SIZE; i++)
-                printf("%d ", deQueue[i]);
+            Display();
             break;
 
         case 5:
@@ -668,4 +419,11 @@ void ORRear()
         }
 
     } while (choice != 5);
+}
+
+void Display()
+{
+    system("clear");
+    for (int i = 0; i < SIZE; i++)
+        printf("%d ", deQueue[i]);
 }
