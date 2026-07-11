874d4d1e533818f8b68921c39f25595e1c0406ae
krishna-tailor <ks.tailor14@gmail.com>
2026-07-11 12:47:04 +0530

    Update Program With Making Functions

--- 06_Double_ended_queue.c
+++ 06_Double_ended_queue.c
@@ -2,30 +2,37 @@
 #include <stdio.h>
 #define SIZE 5
 
+int front = -1, rear = -1, queue[SIZE], element;
+
+void insertf();
+void insertr();
+
+void deletef();
+void deleter();
+
+void outputRestricted();
 void inputRestricted();
+
 void insertFromFront();
 void insertFromRear();
 
-void outputRestricted();
 void deleteFromFront();
 void deleteFromRear();
 
 int main()
 {
-    system("clear");
     int choice;
-
+    system("clear");
     do
     {
-        printf("==== Double Ended Queue Operations ====\n");
-        printf("---------------------------------------\n");
-        printf("1. Input Restricted \n2. Output Restricted\n3. Exit Program");
-        printf("\nEnter Your Choice: ");
+        printf("===== Double Ended Queue Operations ===== \n");
+        printf("----------------------------------------- \n");
+        printf("1. Input Restricted \n2. Output restricted \n3. Exit Program\n");
+        printf("Enter Your Choice: ");
         scanf("%d", &choice);
 
         switch (choice)
         {
-
         case 1:
             system("clear");
             inputRestricted();
@@ -43,10 +50,102 @@ int main()
 
         default:
             system("clear");
-            printf("==== Invalid Choice ====\n");
+            printf("==== Invalid Choice ====\n\n");
             break;
         }
     } while (choice != 3);
+}
+
+void insertf()
+{
+    if ((front == 0 && rear == SIZE - 1) || (front == rear + 1))
+    {
+        printf("== == == Queue Is OvferFlow == == ==\n\n");
+        return;
+    }
+
+    if (front == -1)
+        front = rear = 0;
+
+    else if (front == 0)
+        front = SIZE - 1;
+
+    else
+        front--;
+
+    printf("Enter Element To Enter: ");
+    scanf("%d", &element);
+
+    queue[front] = element;
+
+    system("clear");
+    printf("==> ==> %d Inserted Sucessfully <== <==\n\n", element);
+}
+
+void insertr()
+{
+    if ((front == 0 && rear == SIZE - 1) || (front == rear + 1))
+    {
+        printf("== == == Queue Is OvferFlow == == ==\n\n");
+        return;
+    }
+
+    if (front == -1)
+        front = rear = 0;
+
+    else if (rear == SIZE - 1)
+        rear = 0;
+
+    else
+        rear++;
+
+    printf("Enter Element To Enter: ");
+    scanf("%d", &element);
+
+    queue[rear] = element;
+
+    system("clear");
+    printf("==> ==> %d Inserted Sucessfully <== <==\n\n", element);
+}
+
+void deletef()
+{
+    if (front == -1)
+    {
+        printf("== == == Queue Is Empty == == ==\n\n");
+        return;
+    }
+
+    printf("==> ==> %d Deleted Sucessfully <== <==\n\n", queue[front]);
+
+    if (front == rear)
+        front = rear = -1;
+
+    else if (front == SIZE - 1)
+        front = 0;
+
+    else
+        front++;
+}
+
+void deleter()
+{
+    if (front == -1)
+    {
+        printf("== == == Queue Is Empty == == ==\n\n");
+        return;
+    }
+
+    printf("==> ==> %d Deleted Sucessfully <== <==\n\n", queue[rear]);
+
+    if (front == rear)
+        front = rear = -1;
+
+    else if (rear == 0)
+        rear = SIZE - 1;
+
+    else
+        rear--;
 }
 
 void inputRestricted()
@@ -55,15 +154,14 @@ void inputRestricted()
 
     do
     {
-        printf("==== Input Restricted Operations ====\n");
-        printf("-------------------------------------\n");
-        printf("1. Insert From Front \n2. Insert From Rear \n3. Return To Main Menu\n");
-        printf("Enter Your Choice: ");
+        printf("===== Input Restricted Operations =====\n");
+        printf("---------------------------------------\n");
+        printf("1. Insert From Front and Delete From Both Sides \n2. Insert From Rear and Delete From Both Sides\n3. Retrun To Main Menu");
+        printf("\nEnter Your Choice: ");
         scanf("%d", &choice);
 
         switch (choice)
         {
-
         case 1:
             system("clear");
             insertFromFront();
@@ -76,12 +174,12 @@ void inputRestricted()
 
         case 3:
             system("clear");
-            printf("=== === === Returned To Main Menu === === ===\n\n");
+            printf("== == == Returned To Main Menu == == ==\n\n");
             break;
 
         default:
             system("clear");
-            printf("==== Invlaid Choice ====\n\n");
+            printf("== == == Invalid Choice == == ==\n\n");
             break;
         }
     } while (choice != 3);
@@ -89,214 +187,96 @@ void inputRestricted()
 
 void insertFromFront()
 {
-    int choice, element;
-    int front = -1, rear = -1, queue[SIZE];
-
+    int choice;
     do
     {
-        printf("==== Insert From Front Operations ====\n");
-        printf("--------------------------------------\n");
-        printf("1. Enter Element \n2. Delete Element From Front \n3. Delete Element From Rear");
-        printf("\n4. Delete Elements And Return To Last Menu\n");
-        printf("\nEnter Your Choice: ");
+        printf("===== Insert From front Operations =====\n");
+        printf("----------------------------------------\n");
+        printf("1. Insert Element \n2. Delete From front \n3. Delete From Rear \n4. Back To Last Menu\n");
+        printf("Enter Your Choice: ");
         scanf("%d", &choice);
 
         switch (choice)
         {
-
         case 1:
             system("clear");
-            // Checking If The Queue Is Full
-            if ((front == 0 && rear == SIZE - 1) || (front == rear + 1))
-            {
-                printf("==> ==> Queue Is Over Flow <== <==\n\n");
-                break;
-            }
-
-            // Updating Index
-            if (front == -1)
-                front = rear = 0;
-
-            else if (front == 0)
-                front = SIZE - 1;
-
-            else
-                front--;
-
-            printf("Enter Your Element: ");
-            scanf("%d", &element);
-
-            queue[front] = element;
-
-            system("clear");
-            printf("==== %d Inserted Sucessfully <====\n\n", element);
+            insertf();
             break;
 
         case 2:
             system("clear");
-            // Checking If The Queue Is Already Empty Or Not
-            if (front == -1)
-            {
-                printf("==> ==> Queue Is Empty <== <==\n\n");
-                break;
-            }
-
-            printf("====> %d Deleted Successfully <====\n\n", queue[front]);
-
-            if (front == rear)
-                front = rear = -1;
-
-            else if (front == SIZE - 1)
-                front = 0;
-
-            else
-                front++;
+            deletef();
             break;
 
         case 3:
             system("clear");
-            // Checking If The Queue Is Already Empty Or Not
-            if (rear == -1)
-            {
-                printf("==> ==> Queue Is Empty <== <==\n\n");
-                break;
-            }
-
-            printf("====> %d Deleted Successfully <====\n\n", queue[rear]);
-
-            if (front == rear)
-                front = rear = -1;
-
-            else if (rear == 0)
-                rear = SIZE - 1;
-
-            else
-                rear--;
+            deleter();
             break;
 
         case 4:
             system("clear");
-            printf("=== === === Returned To Last Menu === === ===\n\n");
+            printf("== == == Returned To Last Menu == == ==\n\n");
             break;
 
         default:
             system("clear");
-            printf("==== Invlaid Choice ====\n\n");
+            printf("== == == Invalid Choice == == ==\n\n");
             break;
         }
-
     } while (choice != 4);
 }
 
 void insertFromRear()
 {
-    int choice, element;
-    int front = -1, rear = -1, queue[SIZE];
+    int choice;
     do
     {
-        printf("==== Insert From Rear Operations ====\n");
-        printf("--------------------------------------\n");
-        printf("1. Enter Element \n2. Delete Element From Front \n3. Delete Element From Rear");
-        printf("\n4. Delete Elements And Return To Last Menu\n");
-        printf("\nEnter Your Choice: ");
+        printf("===== Insert From Rear Operations =====\n");
+        printf("----------------------------------------\n");
+        printf("1. Insert Element \n2. Delete From front \n3. Delete From Rear \n4. Back To Last Menu\n");
+        printf("Enter Your Choice: ");
         scanf("%d", &choice);
 
         switch (choice)
         {
-
         case 1:
             system("clear");
-            // Checking If The Queue Is Full
-            if ((front == 0 && rear == SIZE - 1) || (front == rear + 1))
-            {
-                printf("==> ==> Queue Is Over Flow <== <==\n\n");
-                break;
-            }
-
-            // Updating Index
-            if (front == -1)
-                front = rear = 0;
-
-            else if (rear == SIZE - 1)
-                rear = 0;
-
-            else
-                rear++;
-
-            printf("Enter Your Element: ");
-            scanf("%d", &element);
-
-            queue[rear] = element;
-
-            system("clear");
-            printf("==== %d Inserted Sucessfully <====\n\n", element);
+            insertr();
             break;
 
         case 2:
             system("clear");
-            // Checking If The Queue Is Already Empty Or Not
-            if (front == -1)
-            {
-                printf("==> ==> Queue Is Empty <== <==\n\n");
-                break;
-            }
-
-            printf("====> %d Deleted Successfully <====\n\n", queue[front]);
-
-            if (front == rear)
-                front = rear = -1;
-
-            else if (front == SIZE - 1)
-                front = 0;
-
-            else
-                front++;
+            deletef();
             break;
 
         case 3:
             system("clear");
-            // Checking If The Queue Is Already Empty Or Not
-            if (rear == -1)
-            {
-                printf("==> ==> Queue Is Empty <== <==\n\n");
-                break;
-            }
-
-            printf("====> %d Deleted Successfully <====\n\n", queue[rear]);
-
-            if (front == rear)
-                front = rear = -1;
-
-            else if (rear == 0)
-                rear = SIZE - 1;
-
-            else
-                rear--;
+            deleter();
             break;
 
         case 4:
             system("clear");
-            printf("=== === === Returned To Last Menu === === ===\n\n");
+            printf("== == == Returned To Last Menu == == ==\n\n");
             break;
 
         default:
             system("clear");
-            printf("==== Invlaid Choice ====\n\n");
+            printf("== == == Invalid Choice == == ==\n\n");
             break;
         }
-
     } while (choice != 4);
 }
 
 void outputRestricted()
 {
     int choice;
+
     do
     {
-        printf("==== Output Restricted Operations ====\n");
-        printf("-------------------------------------\n");
-        printf("1. Delete From Front \n2. Delete From Rear \n3. Return To Main Menu\n");
-        printf("Enter Your Choice: ");
+        printf("===== Output Restricted Operations =====\n");
+        printf("---------------------------------------\n");
+        printf("1. Delete From Front And Enter Elements From Both Sides \n2. Delete From Rear And Enter Elements From Both Sides\n3. Retrun To Main Menu");
+        printf("\nEnter Your Choice: ");
         scanf("%d", &choice);
 
         switch (choice)
@@ -313,12 +293,12 @@ void outputRestricted()
 
         case 3:
             system("clear");
-            printf("=== === === Returned To Main Menu === === ===\n\n");
+            printf("== == == Returned To Main Menu == == ==\n\n");
             break;
 
         default:
             system("clear");
-            printf("==== Invlaid Choice ====\n\n");
+            printf("== == == Invalid Choice == == ==\n\n");
             break;
         }
     } while (choice != 3);
@@ -326,105 +306,40 @@ void outputRestricted()
 
 void deleteFromFront()
 {
-    int choice, element;
-    int front = -1, rear = -1, queue[SIZE];
-
+    int choice;
     do
     {
-        printf("==== Delete From Front Operations ====\n");
-        printf("--------------------------------------\n");
-        printf("1. Insert Element From Front \n2. Insert Element From Rear \n3. Delete Element");
-        printf("\n4. Delete Elements And Return To Last Menu\n");
-        printf("\nEnter Your Choice: ");
+        printf("===== Delete From Front Operations =====\n");
+        printf("----------------------------------------\n");
+        printf("1. Insert Element From Front \n2. Insert From Rear \n3. Delete Element \n4. Back To Last Menu\n");
+        printf("Enter Your Choice: ");
         scanf("%d", &choice);
 
         switch (choice)
         {
         case 1:
             system("clear");
-            // Checking If The Queue Is Full
-            if ((front == 0 && rear == SIZE - 1) || (front == rear + 1))
-            {
-                printf("==> ==> Queue Is Over Flow <== <==\n\n");
-                break;
-            }
-
-            // Updating Index
-            if (front == -1)
-                front = rear = 0;
-
-            else if (front == 0)
-                front = SIZE - 1;
-
-            else
-                front--;
-
-            printf("Enter Your Element: ");
-            scanf("%d", &element);
-
-            queue[front] = element;
-
-            system("clear");
-            printf("==== %d Inserted Sucessfully <====\n\n", element);
+            insertf();
             break;
 
         case 2:
             system("clear");
-            // Checking If The Queue Is Full
-            if ((front == 0 && rear == SIZE - 1) || (front == rear + 1))
-            {
-                printf("==> ==> Queue Is Over Flow <== <==\n\n");
-                break;
-            }
-
-            // Updating Index
-            if (front == -1)
-                front = rear = 0;
-
-            else if (rear == SIZE - 1)
-                rear = 0;
-
-            else
-                rear++;
-
-            printf("Enter Your Element: ");
-            scanf("%d", &element);
-
-            queue[rear] = element;
-
-            system("clear");
-            printf("==== %d Inserted Sucessfully <====\n\n", element);
+            insertr();
             break;
 
         case 3:
             system("clear");
-            // Checking If The Queue Is Already Empty Or Not
-            if (front == -1)
-            {
-                printf("==> ==> Queue Is Empty <== <==\n\n");
-                break;
-            }
-
-            printf("====> %d Deleted Successfully <====\n\n", queue[front]);
-
-            if (front == rear)
-                front = rear = -1;
-
-            else if (front == SIZE - 1)
-                front = 0;
-
-            else
-                front++;
+            deletef();
             break;
 
         case 4:
             system("clear");
-            printf("=== === === Returned To Last Menu === === ===\n\n");
+            printf("== == == Returned To Last Menu == == ==\n\n");
             break;
 
         default:
             system("clear");
-            printf("==== Invlaid Choice ====\n\n");
+            printf("== == == Invalid Choice == == ==\n\n");
             break;
         }
     } while (choice != 4);
@@ -432,105 +347,40 @@ void deleteFromFront()
 
 void deleteFromRear()
 {
-    int choice, element;
-    int front = -1, rear = -1, queue[SIZE];
-
+    int choice;
     do
     {
-        printf("==== Delete From Front Operations ====\n");
-        printf("--------------------------------------\n");
-        printf("1. Insert Element From Front \n2. Insert Element From Rear \n3. Delete Element");
-        printf("\n4. Delete Elements And Return To Last Menu\n");
-        printf("\nEnter Your Choice: ");
+        printf("===== Delete From Rear Operations =====\n");
+        printf("----------------------------------------\n");
+        printf("1. Insert Element From Front \n2. Insert From Rear \n3. Delete Element \n4. Back To Last Menu\n");
+        printf("Enter Your Choice: ");
         scanf("%d", &choice);
 
         switch (choice)
         {
         case 1:
             system("clear");
-            // Checking If The Queue Is Full
-            if ((front == 0 && rear == SIZE - 1) || (front == rear + 1))
-            {
-                printf("==> ==> Queue Is Over Flow <== <==\n\n");
-                break;
-            }
-
-            // Updating Index
-            if (front == -1)
-                front = rear = 0;
-
-            else if (front == 0)
-                front = SIZE - 1;
-
-            else
-                front--;
-
-            printf("Enter Your Element: ");
-            scanf("%d", &element);
-
-            queue[front] = element;
-
-            system("clear");
-            printf("==== %d Inserted Sucessfully <====\n\n", element);
+            insertf();
             break;
 
         case 2:
             system("clear");
-            // Checking If The Queue Is Full
-            if ((front == 0 && rear == SIZE - 1) || (front == rear + 1))
-            {
-                printf("==> ==> Queue Is Over Flow <== <==\n\n");
-                break;
-            }
-
-            // Updating Index
-            if (front == -1)
-                front = rear = 0;
-
-            else if (rear == SIZE - 1)
-                front = 0;
-
-            else
-                rear++;
-
-            printf("Enter Your Element: ");
-            scanf("%d", &element);
-
-            queue[rear] = element;
-
-            system("clear");
-            printf("==== %d Inserted Sucessfully <====\n\n", element);
+            insertr();
             break;
 
         case 3:
             system("clear");
-            // Checking If The Queue Is Already Empty Or Not
-            if (rear == -1)
-            {
-                printf("==> ==> Queue Is Empty <== <==\n\n");
-                break;
-            }
-
-            printf("====> %d Deleted Successfully <====\n\n", queue[rear]);
-
-            if (front == rear)
-                front = rear = -1;
-
-            else if (rear == 0)
-                rear = SIZE - 1;
-
-            else
-                rear--;
+            deleter();
             break;
 
         case 4:
             system("clear");
-            printf("=== === === Returned To Last Menu === === ===\n\n");
+            printf("== == == Returned To Last Menu == == ==\n\n");
             break;
 
         default:
             system("clear");
-            printf("==== Invlaid Choice ====\n\n");
+            printf("== == == Invalid Choice == == ==\n\n");
             break;
         }
     } while (choice != 4);
